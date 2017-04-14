12b382f04085ccfc6366bbb834ec5731cd247980
martinbobw <martin.bob.w@gmail.com>
2017-04-14 10:11:04 -0400

    2nd round of edits, use PNG not PDF, use bootstrap

--- server.R
+++ server.R
@@ -9,13 +9,42 @@ library(tmap)
 library(tmaptools)
 library(RColorBrewer)
 library(scales)
+library(ggplot2)
 legendTitle <- "Assaults per 100 Officers"
-stateColClasses <- c(rep("character", 7), rep("integer", 7),  rep("numeric", 2))
-stateAbbrNbrs <- read.csv("P3 state abbr.csv", stringsAsFactors = FALSE,
+stateColClasses <- c("character", rep("integer", 2), rep("character", 6),
+                     rep("integer", 4))
+stateAbbrNbrs <- read.csv("P3 state abbr orig.csv", stringsAsFactors = FALSE,
                           colClasses = stateColClasses)
 stateAbbrNbrs$officers100 <- stateAbbrNbrs$NbrOfOfficers / 100
 stateAbbrNbrs$officerAssault100 <- round((stateAbbrNbrs$OfficersAssaulted /
-                                         stateAbbrNbrs$officers100), 1)
+                                            stateAbbrNbrs$officers100), 1)
+stateAbbrNbrs$regOfficers100 <- stateAbbrNbrs$RegionOfficers / 100
+stateAbbrNbrs$RegionAssaultsPer100 <- round((stateAbbrNbrs$RegionAssaults /
+                                               stateAbbrNbrs$regOfficers100), 1)
+stateAbbrNbrs$divOfficers100 <- stateAbbrNbrs$DivisionOfficers / 100
+stateAbbrNbrs$DivisionAssaultsPer100 <- round((stateAbbrNbrs$DivisionAssaults /
+                                                 stateAbbrNbrs$divOfficers100), 1)
+stateAbbrNbrsNoNA <- stateAbbrNbrs[!is.na(stateAbbrNbrs$OfficersAssaulted), ]
+# could do this in a compound OR statement but doing it in 2
+stateAbbrNbrsNoNA <- stateAbbrNbrsNoNA[!is.na(stateAbbrNbrsNoNA$NbrOfOfficers), ]
+# FIPS codes for all 50 states and DC
+midwestFIPS <- c("17", "18", "19", "20", "26", "27", "29", "31", "38", "39",
+                 "46", "55")
+northeastFIPS <- c("09", "23", "25", "33", "34", "36", "42", "44", "50")
+southFIPS <- c("01", "05", "10", "11", "12", "13", "21", "22", "24", "28",
+               "37", "40", "45", "47", "48", "51", "54")
+westFIPS <- c("02", "04", "06", "08", "15", "16", "30", "32", "35", "41",
+              "49", "53", "56")
+statesDCFIPS <- c(midwestFIPS, northeastFIPS, southFIPS, westFIPS)
+regionNumbers <- unique(stateAbbrNbrs[stateAbbrNbrs$FIPSCode %in% statesDCFIPS,
+                                      c("Region", "RegionAssaultsPer100")])
+# These FIPS codes below indicate the states that have data for 1997
+midwestFIPS1 <- c("18", "19", "26", "27", "29", "31", "38", "39", "46", "55")
+northeastFIPS1 <- c("09", "23", "25", "33", "34", "36", "42", "44")
+southFIPS1 <- c("01", "05", "10", "11", "13", "21", "22", "24", "28",
+                "37", "40", "45", "47", "48", "51", "54")
+westFIPS1 <- c("02", "04", "06", "08", "15", "16", "30", "32", "35", "41",
+               "49", "53", "56")
 usshapefile <- "cb_2015_us_state_5m.shp"
 # Read in the shapefile for US states:
 usgeo <- read_shape(file=usshapefile)
@@ -26,16 +55,21 @@ stateAbbrNbrs <- stateAbbrNbrs[order(stateAbbrNbrs$StateAbbr), ]
 identical(factor(stateAbbrNbrs$StateAbbr), usgeo@data$STUSPS)
 usmap <- append_data(usgeo, stateAbbrNbrs,
                      key.shp = "STUSPS", key.data="StateAbbr")
-midwestFIPS <- c("17", "18", "19", "20", "26", "27", "29", "31", "38", "39",
-                 "46", "55")
-northeastFIPS <- c("09", "23", "25", "33", "34", "36", "42", "44", "50")
-southFIPS <- c("01", "05", "10", "11", "12", "13", "21", "22", "24", "28",
-               "37", "40", "45", "47", "48", "51", "54")
-westFIPS <- c("02", "04", "06", "08", "15", "16", "30", "32", "35", "41",
-              "49", "53", "56")
 palGreens <- colorNumeric(palette = c("#C7E9C0", "#A1D99B", "#74C476", "#41AB5D",
                                       "#238B45", "#006D2C", "#00441B"),
                           domain = usmap@data$officerAssault100)
+palMidwest <- colorNumeric(palette = c("#BACDC6", "#98B499", "#678674",
+                                       "#4C5C58", "#3C3C68", "#8987CC"),
+                           domain = usmap@data$officerAssault100)
+palNortheast <- colorNumeric(palette = c("#E0E7E0", "#F1E3D2", "#FE97BE",
+                                         "#E65B51", "#2A5F9E", "#6FB882"),
+                           domain = usmap@data$officerAssault100)
+palSouth <- colorNumeric(palette = c("#F6EDEA", "#F3DBD3", "#F8A28C",
+                                     "#8F7B69", "#8FA67A", "#EDE0A6"),
+                             domain = usmap@data$officerAssault100)
+palWest <- colorNumeric(palette = c("#95C4B2", "#6CB284", "#427951",
+                                    "#295333", "#103A25", "#222D26"),
+                         domain = usmap@data$officerAssault100)
 midwestmap <- usmap[usmap@data$STATEFP %in% midwestFIPS, ]
 midwestpopup <- paste0("<b>State: ", midwestmap@data$NAME,
                        "</b><br />Assaults: ",
@@ -72,78 +106,131 @@ us1popup <- paste0("<b>Region: ", us1map@data$Region,
                     "</b><br />Assaults per 100 Officers: ",
                     us1map@data$RegionAssaultsPer100)
 #binPalRdYlBu <- colorBin(palette = "RdYlBu",
-#binGreens <- colorBin(palette = "Greens",
-#                      domain = us1map@data$RegionAssaultsPer100,
-#                      8, pretty = FALSE)
+binGreens <- colorBin(palette = "Greens",
+                      domain = us1map@data$RegionAssaultsPer100,
+                      8, pretty = FALSE)
 shinyServer(function(input, output) {
 
-#  output$about <- renderImage({
-#    filename <- normalizePath(file.path('./images', "P32 police image copy.jpg"))
-#    filename <- normalizePath(file.path('./images', "P33 cops.pdf"))
-#    list(src = filename)
-#  }, deleteFile = FALSE)
-  
+      output$midwestBar <- renderPlot(ggplot(data = stateAbbrNbrs[stateAbbrNbrs$FIPSCode %in% midwestFIPS1, ],
+                                             aes(x = reorder(StateAbbr, -officerAssault100),
+                                                 y = officerAssault100)) +
+                                        geom_bar(stat = "identity", fill = "#98B499") +
+                                        ggtitle("Officers Assaulted by State - Midwest") +
+                                        xlab("State") +
+                                        ylab("Assaults per 100 Officers") +
+                                        theme(panel.grid.major.x = element_blank(),
+                                              panel.grid.major = element_line(color = "grey60"),
+                                              panel.grid.minor = element_line(color = "grey40"),
+                                              panel.background = element_rect(fill = "white"),
+                                              axis.ticks = element_blank()))
       output$midwest <- renderLeaflet(leaflet(midwestmap) %>%
                                     addProviderTiles("CartoDB.Positron") %>%
                                     addPolygons(stroke=FALSE, 
                                                 smoothFactor = 0.2, 
                                                 fillOpacity = .3, 
                                                 popup=midwestpopup, 
-                                                color= ~palGreens(midwestmap@data$officerAssault100)
+                                                color= ~palMidwest(midwestmap@data$officerAssault100)
                                     ) %>% fitBounds(-102.5, 36.5, -79, 49.5)
                                       %>% addLegend("bottomright",
                                                     title = legendTitle,
-                                                    pal = palGreens,
+                                                    pal = palMidwest,
                                                     values = ~usmap@data$officerAssault100,
                                                     opacity = 0.8
                                                     )
                                    )
+      output$northeastBar <- renderPlot(ggplot(data = stateAbbrNbrs[stateAbbrNbrs$FIPSCode %in% northeastFIPS1, ],
+                                             aes(x = reorder(StateAbbr, -officerAssault100),
+                                                 y = officerAssault100)) +
+                                        geom_bar(stat = "identity", fill = "#F1E3D2") +
+                                        ggtitle("Officers Assaulted by State - Northeast") +
+                                        xlab("State") +
+                                        ylab("Assaults per 100 Officers") +
+                                        theme(panel.grid.major.x = element_blank(),
+                                              panel.grid.major = element_line(color = "grey60"),
+                                              panel.grid.minor = element_line(color = "grey40"),
+                                              panel.background = element_rect(fill = "white"),
+                                              axis.ticks = element_blank()))
       output$northeast <- renderLeaflet(leaflet(northeastmap) %>%
                                         addProviderTiles("CartoDB.Positron") %>%
                                         addPolygons(stroke=FALSE, 
                                                     smoothFactor = 0.2, 
                                                     fillOpacity = .3, 
                                                     popup=northeastpopup, 
-                                                    color= ~palGreens(northeastmap@data$officerAssault100))
+                                                    color= ~palNortheast(northeastmap@data$officerAssault100))
                                         %>% fitBounds(-81, 39, -67, 48)
-#                                        %>% setView(-80, 39, zoom = 5)
                                       %>% addLegend("bottomright",
                                                     title = legendTitle,
-                                                    pal = palGreens,
+                                                    pal = palNortheast,
                                                     values = ~usmap@data$officerAssault100,
                                                     opacity = 0.8
                                       )
       )
+      output$southBar <- renderPlot(ggplot(data = stateAbbrNbrs[stateAbbrNbrs$FIPSCode %in% southFIPS1, ],
+                                               aes(x = reorder(StateAbbr, -officerAssault100),
+                                                   y = officerAssault100)) +
+                                          geom_bar(stat = "identity", fill = "#F3DBD3") +
+                                          ggtitle("Officers Assaulted by State - South") +
+                                          xlab("State") +
+                                          ylab("Assaults per 100 Officers") +
+                                          theme(panel.grid.major.x = element_blank(),
+                                                panel.grid.major = element_line(color = "grey60"),
+                                                panel.grid.minor = element_line(color = "grey40"),
+                                                panel.background = element_rect(fill = "white"),
+                                                axis.ticks = element_blank()))
       output$south <- renderLeaflet(leaflet(southmap) %>%
                                           addProviderTiles("CartoDB.Positron") %>%
                                           addPolygons(stroke=FALSE, 
                                                       smoothFactor = 0.2, 
                                                       fillOpacity = .3, 
                                                       popup=southpopup, 
-                                                      color= ~palGreens(southmap@data$officerAssault100))
+                                                      color= ~palSouth(southmap@data$officerAssault100))
                                         %>% fitBounds(-107, 24, -75, 40)
                                         %>% addLegend("bottomright",
                                                       title = legendTitle,
-                                                      pal = palGreens,
+                                                      pal = palSouth,
                                                       values = ~usmap@data$officerAssault100,
                                                       opacity = 0.8
                                         )
       )
+      output$westBar <- renderPlot(ggplot(data = stateAbbrNbrs[stateAbbrNbrs$FIPSCode %in% westFIPS1, ],
+                                           aes(x = reorder(StateAbbr, -officerAssault100),
+                                               y = officerAssault100)) +
+                                      geom_bar(stat = "identity", fill = "#6CB284") +
+                                      ggtitle("Officers Assaulted by State - West") +
+                                      xlab("State") +
+                                      ylab("Assaults per 100 Officers") +
+                                      theme(panel.grid.major.x = element_blank(),
+                                            panel.grid.major = element_line(color = "grey60"),
+                                            panel.grid.minor = element_line(color = "grey40"),
+                                            panel.background = element_rect(fill = "white"),
+                                            axis.ticks = element_blank()))
       output$west <- renderLeaflet(leaflet(westmap) %>%
                                       addProviderTiles("CartoDB.Positron") %>%
                                       addPolygons(stroke=FALSE, 
                                                   smoothFactor = 0.2, 
                                                   fillOpacity = .3, 
                                                   popup=westpopup, 
-                                                  color= ~palGreens(westmap@data$officerAssault100))
+                                                  color= ~palWest(westmap@data$officerAssault100))
                                     %>% fitBounds(-125, 30, -102, 49)
                                     %>% addLegend("bottomright",
                                                   title = legendTitle,
-                                                  pal = palGreens,
+                                                  pal = palWest,
                                                   values = ~usmap@data$officerAssault100,
                                                   opacity = 0.8
                                     )
       )
+      output$wholeBar <- renderPlot(ggplot(data = regionNumbers,
+                                          aes(x = reorder(Region, -RegionAssaultsPer100),
+                                              y = RegionAssaultsPer100)) +
+                                     geom_bar(stat = "identity", fill = "#6CB284") +
+                                     ggtitle("Officers Assaulted By Region") +
+                                     xlab("Region") +
+                                     ylab("Assaults per 100 Officers") +
+                                     theme(panel.grid.major.x = element_blank(),
+                                           panel.grid.major = element_line(color = "grey60"),
+                                           panel.grid.minor = element_line(color = "grey40"),
+                                           panel.background = element_rect(fill = "white"),
+                                           axis.ticks = element_blank()))
       output$wholeus <- renderLeaflet(leaflet(us1map) %>%
                                      addProviderTiles("CartoDB.Positron") %>%
                                      addPolygons(stroke = FALSE, 
@@ -158,7 +245,7 @@ shinyServer(function(input, output) {
                                                  pal = palGreens,
                                                  values = ~us1map@data$RegionAssaultsPer100,
                                                  opacity = 0.8
-                                   )
+                                                )
       )
 #  output$conclusion <- renderImage({
 #    filename <- normalizePath(file.path('./images', "MapOfBostonBPD.png"))
--- ui.R
+++ ui.R
@@ -8,9 +8,9 @@
 library(shiny)
 library(leaflet)
 shinyUI(pageWithSidebar(
-  headerPanel("Law Enforcement Officers Killed and Assaulted"),
+  headerPanel("Law Enforcement Officers Assaulted - 1997"),
   sidebarPanel(
-      conditionalPanel(condition="input.tabselected==1",
+      conditionalPanel(condition = "input.tabselected==1",
                        h4("About"),
                        p(),
                        p("The Law Enforcement Officers Killed and Assaulted (LEOKA)
@@ -34,57 +34,75 @@ shinyUI(pageWithSidebar(
                          hypothesis will be tested using known statistical methods.")),
       conditionalPanel(condition="input.tabselected==2",
                        h4("Midwestern Region"),
-                       p("Click inside a state to see assault statistics")),
-      conditionalPanel(condition="input.tabselected==3",
-                       h4("Northeastern Region"),
+                       p(),
                        p("Click inside a state to see assault statistics"),
                        p(),
                        p(),
-                       p("Zoom or pan to see smaller states")),
+                       p(),
+                       plotOutput("midwestBar")),
+      conditionalPanel(condition="input.tabselected==3",
+                       h4("Northeastern Region"),
+                       p(),
+                       p("Click inside a state to see assault statistics"),
+                       p(),
+                       p(),
+                       p("Zoom or pan to see smaller states"),
+                       plotOutput("northeastBar")),
       conditionalPanel(condition="input.tabselected==4",
                        h4("Southern Region"),
                        p("Click inside a state to see assault statistics"),
                        p(),
                        p(),
-                       p("Zoom or pan to see smaller states")),
+                       p("Zoom or pan to see smaller states"),
+                       plotOutput("southBar")),
       conditionalPanel(condition="input.tabselected==5",
                        h4("Western Region"),
                        p("Click inside a state to see assault statistics"),
                        p(),
                        p(),
-                       p("Zoom or pan to see Alaska and Hawaii")),
+                       p("Zoom or pan to see Alaska and Hawaii"),
+                       plotOutput("westBar")),
       conditionalPanel(condition="input.tabselected==6",
                        h4("Methodology"),
                        p(),
-                       p(""),
-                       p(),
+                       p("The original agency level data set was cleaned to
+                         remove police agencies that did not report the number
+                         of officers or that did not report full year assaults.
+                         Assaults were then summarized at the state and 
+                         regional levels."),
                        h4("Results"),
                        p(),
                        p("Through statistical analysis, the four regions'
                          average number of assaults per 100 officers have
                          been compared to one another. There is no significant
-                         difference in the average number of assaults per 100
-                         officers between U.S. regions."),
-                       p(),
+                         difference between U.S. regions."),
                        p(),
                        p("Click inside a state to see assault statistics for that
-                         U.S. region. Note the color scale has shifted from state
-                         level to region level."),
-                       p(),
-                       p(),
-                       p("Zoom or pan to see Alaska and Hawaii."))
+                         U.S. region. Zoom or pan to see Alaska and Hawaii."),
+                       plotOutput("wholeBar"))
     ),
   mainPanel(
       tabsetPanel(
-        tabPanel("Introduction", value = 1, tags$iframe(src="https://www.dropbox.com/s/coqiwi49vf3layf/P33%20Tab%201.pdf?raw=1",
-                                                        width = "900", height = "600")),
-        tabPanel("Midwest", value = 2, leafletOutput("midwest")),
-        tabPanel("Northeast", value = 3, leafletOutput("northeast")),
-        tabPanel("South", value = 4, leafletOutput("south")),
-        tabPanel("West", value = 5, leafletOutput("west")),
-        tabPanel("Conclusion", value = 6, leafletOutput("wholeus")),
+        tabPanel("Introduction", value = 1, img(src = "P33_Tab_1.png", height = "600px",
+                                                width = "900px"),
+#         tabPanel("Introduction", value = 1, img(src = "/Users/robertmartin/Desktop/Northeastern/PPUA 6302 Info Design Visual Analytics/P33_Martin_R/images/P33_Tab_1.png",
+#                                                 height = "600px"),
+#        tabPanel("Introduction", value = 1, tags$iframe(src="https://www.dropbox.com/s/coqiwi49vf3layf/P33%20Tab%201.pdf?raw=1",
+#                                                        width = "900", height = "600"),
+                 includeCSS(path = "www/bootstrap.css")),
+        tabPanel("Midwest", value = 2, 
+                 leafletOutput("midwest", height = "600px")),
+        tabPanel("Northeast", value = 3, leafletOutput("northeast",
+                                                       height = "600px")),
+        tabPanel("South", value = 4, leafletOutput("south",
+                                                   height = "600px")),
+        tabPanel("West", value = 5, leafletOutput("west",
+                                                  height = "600px")),
+        tabPanel("Conclusion", value = 6, leafletOutput("wholeus",
+                                                        height = "600px")),
         id = "tabselected"
-      )
+      ),
+      style = 'height:600px'
     )
   )
 )
